eb0c960976952cc3fbf845156372059c37f6c7ed
Your Name <josefdaly@gmail.com>
2015-10-02 00:25:29 -0700

    Multiple channels now supported. Turned sockets storage array into hash

--- app.rb
+++ app.rb
@@ -2,23 +2,33 @@ require 'sinatra'
 require 'sinatra-websocket'
 
 set :server, 'thin'
-set :sockets, []
+set :sockets, {}
 
 get '/' do
+  "Setup a chatroom by extending any path off the domain name and sharing the url
+   with your friend."
+end
+
+get '/:path' do
+  path = params[:path]
   if !request.websocket?
     erb :index
   else
     request.websocket do |ws|
       ws.onopen do
-        ws.send("Hello World!")
-        settings.sockets << ws
+        ws.send("Hello from the server!")
+        if settings.sockets[path]
+          settings.sockets[path].push(ws)
+        else
+          settings.sockets[path] = [ws]
+        end
       end
       ws.onmessage do |msg|
-        EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+        EM.next_tick { settings.sockets[path].each{ |s| s.send(msg) } }
       end
       ws.onclose do
         warn("websocket closed")
-        settings.sockets.delete(ws)
+        settings.sockets[path].delete(ws)
       end
     end
   end
