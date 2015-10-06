86e0d0d7ea46a95d62ba8ea3ea63e53a8ebe7e23
Your Name <josefdaly@gmail.com>
2015-10-06 14:23:56 -0700

    Wrap controller logic in class

--- app.rb
+++ app.rb
@@ -1,34 +1,38 @@
 require 'sinatra'
 require 'sinatra-websocket'
 
-set :server, 'thin'
-set :sockets, {}
 
-get '/' do
-  "Setup a chatroom by extending any path off the domain name and sharing the url
-   with your friend."
-end
+class ChatApp < Sinatra::Application
 
-get '/:path' do
-  path = params[:path]
-  if !request.websocket?
-    erb :index
-  else
-    request.websocket do |ws|
-      ws.onopen do
-        ws.send("Hello from the server!")
-        if settings.sockets[path]
-          settings.sockets[path].push(ws)
-        else
-          settings.sockets[path] = [ws]
+  set :server, 'thin'
+  set :sockets, {}
+
+  get '/' do
+    "Setup a chatroom by extending any path off the domain name and sharing the url
+     with your friend."
+  end
+
+  get '/:path' do
+    path = params[:path]
+    if !request.websocket?
+      erb :index
+    else
+      request.websocket do |ws|
+        ws.onopen do
+          ws.send("Hello from the server!")
+          if settings.sockets[path]
+            settings.sockets[path].push(ws)
+          else
+            settings.sockets[path] = [ws]
+          end
         end
-      end
-      ws.onmessage do |msg|
-        EM.next_tick { settings.sockets[path].each{ |s| s.send(msg) } }
-      end
-      ws.onclose do
-        warn("websocket closed")
-        settings.sockets[path].delete(ws)
+        ws.onmessage do |msg|
+          EM.next_tick { settings.sockets[path].each{ |s| s.send(msg) } }
+        end
+        ws.onclose do
+          warn("websocket closed")
+          settings.sockets[path].delete(ws)
+        end
       end
     end
   end
